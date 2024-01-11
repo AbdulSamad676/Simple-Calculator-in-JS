72e1b1f2aecf45f4a7473180aaff4303035d86b3
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-11 15:23:19 +0500

    JS functionality adding

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,9 @@
 let displayResult = document.querySelector('.display');
 console.log(displayResult);
+
+let result = eval('24*6');
+// console.log(result);
+
+let btns = document.querySelectorAll('.Btn');
+
+console.log(btns);
