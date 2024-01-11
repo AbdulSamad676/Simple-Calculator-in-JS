f5724393c97d0e683fcc1b9e4970a3a3e14b3a7a
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-11 15:45:27 +0500

    arithmatic operation deno

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,29 @@
-let displayResult = document.querySelector('.display');
-console.log(displayResult);
+let displayScreen = document.querySelector('.display');
+console.log(displayScreen);
 
-let result = eval('24*6');
+// let result = eval('24*6');
 // console.log(result);
 
 let btns = document.querySelectorAll('.Btn');
 
 console.log(btns);
+btns.forEach(btn => {
+	btn.addEventListener('click', function (e) {
+		console.log(e.target.innerHTML);
+		if (e.target.innerHTML === 'x') {
+			e.target.innerHTML = '*';
+			displayScreen.innerHTML += e.target.innerHTML;
+		} else {
+			displayScreen.innerHTML += e.target.innerHTML;
+			// displayScreen.innerHTML = displayScreen.value;
+		}
+	});
+});
+
+function calculateResult() {
+	let displayData = eval(displayScreen.innerHTML);
+	displayScreen.innerHTML = displayData;
+}
+function clearScreen() {
+	displayScreen.innerHTML = '';
+}
